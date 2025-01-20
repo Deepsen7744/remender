f4fa1c5ee24771733787537b67ad5898f15754f4
Deep sen <deepsen771@gmail.com>
2025-01-20 22:10:17 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
+// const { sendBasicEmail } = require('./services/email-service')
+const cron = require('node-cron')
 
 const setupAndStartServer = () => {
   const app = express()
@@ -11,6 +13,15 @@ const setupAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`)
+    // sendBasicEmail(
+    //   'bunusen730@gmail.com',
+    //   '2022pgcsca037@nitjsr.ac.in',
+    //   'this is testing mail',
+    //   'hy how are you'
+    // )
+    cron.schedule('*/2 * * * *', () => {
+      console.log('running a task every two minutes')
+    })
   })
 }
 
